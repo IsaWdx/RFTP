691846dba2db4b4c19492d247457d3374b11647e
IsaWdx <isachriswang@163.com>
2015-10-13 20:59:08 +0800

    through a reliable channel

--- Reliable-File-Transfer-Protocal/src/FileReceiver.java
+++ Reliable-File-Transfer-Protocal/src/FileReceiver.java
@@ -7,12 +7,13 @@ import java.nio.*;
 import java.util.zip.*;
 
 public class FileReceiver {
-	//total length of header: 22bytes
+	//total length of header: 24bytes
 	//the long field
 	private static long checksum;
 	//the int field
 	private static int ack_num;
 	private static int sequence;
+	private static int start_sequence;
 	//the short field
 	private static short length;
 	private static int window;
@@ -24,21 +25,27 @@ public class FileReceiver {
 	private static int syn;
 	private static int title ;
 	private static int end;
+	private static int end_sequence;
 
 	//for buffering the received
-	private static Map<Integer, Integer>emptyarea;
+	private static Map<Integer, Integer>window_buffer;//sequence/content-length
 	private static byte[] windowbyte;
 	private static ByteBuffer bb;
 
     //shared resources
-	private static int PKT_SIZE = 980;
-	private static int WD_SIZE = PKT_SIZE*5;
+	private static int PKT_SIZE ;
+	private static int WD_SIZE ;
 	private static byte[] reply;
 	private static String filename;
 	private static FileOutputStream fos;
 	public static void main(String[] args) throws Exception 
 	{
-		emptyarea.add(new Pair<Integer,Integer>(0,WD_SIZE));
+		window_buffer = new HashMap<Integer, Integer>();
+		PKT_SIZE = 800;
+		WD_SIZE = PKT_SIZE*5;
+		end_sequence = -1;
+		start_sequence = 0;
+		ack_num = 0;
 		if (args.length != 1) {
 			System.err.println("Usage: FileReceiver <port>");
 			System.exit(-1);
@@ -48,7 +55,7 @@ public class FileReceiver {
 		bb = ByteBuffer.wrap(windowbyte);
 		int port = Integer.parseInt(args[0]);
 		DatagramSocket sk = new DatagramSocket(port);
-		byte[] data = new byte[1500];
+		byte[] data = new byte[PKT_SIZE+24];
 		DatagramPacket pkt = new DatagramPacket(data, data.length);
 		ByteBuffer b = ByteBuffer.wrap(data);
 		CRC32 crc = new CRC32();
@@ -56,7 +63,7 @@ public class FileReceiver {
 		{
 			pkt.setLength(data.length);
 			sk.receive(pkt);
-			if (pkt.getLength() < 8)
+			if (pkt.getLength() < 24)
 			{
 				System.out.println("Pkt too short");
 				continue;
@@ -71,30 +78,86 @@ public class FileReceiver {
 			{
 				System.out.println("Pkt corrupt");
 			}
-			else
-			{
+			else {
 				sequence = b.getInt();
-				ack_num = b.getInt();
+				int tmp = b.getInt();
 				int window_and_bits = b.getInt();
-				end = (window_and_bits&0x10000)>>16;
-				title = (window_and_bits&0x20000)>>17;
-				fin = (window_and_bits&0x40000)>>18;
-				syn = (window_and_bits&0x80000)>>19;
-				ack = (window_and_bits&0x100000)>>20;
-				window = (window_and_bits&0xffff);
-                content_length = b.getInt();
-				if(title == 1){
-					byte[] name = Arrays.copyOfRange(data, 22, content_length);
-					filename = name.toString();
+				end = (window_and_bits & 0x10000) >> 16;
+				title = (window_and_bits & 0x20000) >> 17;
+				fin = (window_and_bits & 0x40000) >> 18;
+				syn = (window_and_bits & 0x80000) >> 19;
+				//ack = (window_and_bits&0x100000)>>20;
+				window = (window_and_bits & 0xffff);
+				content_length = b.getInt();
+
+
+				if (title == 1) {
+					byte[] name = Arrays.copyOfRange(data, 24, content_length+24);
+					System.out.println(new String(name));
+					System.out.println("content-length"+content_length);
+					filename = new String(name);
 					fos = new FileOutputStream(filename);
+					start_sequence = content_length;
+					ack_num = content_length;
+				} else {
+
+
+					if(fos == null)continue;
+					if (window_buffer.containsKey(sequence)) {//discard duplicate
+						;
+					} else if (end!=1&&(sequence == ack_num) && (sequence - start_sequence + content_length <= WD_SIZE)) {//put in sequentially
+						byte[]newbyte = Arrays.copyOfRange(data, 24, content_length + 24);
+						System.arraycopy(newbyte, 0, windowbyte, sequence - start_sequence, content_length);
+						System.out.println("sequence"+sequence);
+						System.out.println("ack_NUM"+ack_num);
+						System.out.println("content_length"+content_length);
+						System.out.println("sequence minus startsequence" + (sequence - start_sequence));
+						System.out.println("WD_SIZE" + WD_SIZE);
+						ack_num += content_length;
+						while (window_buffer.containsKey(ack_num)) {
+							ack_num += window_buffer.get(ack_num);
+						}//if there are bytes occupied behind
+						window_buffer.put(sequence, content_length);
+					} else if (end!=1&&(sequence == ack_num) &&( sequence - start_sequence + content_length > WD_SIZE)) {//output + put in sequentially + update ack and sequence and buffer
+						System.out.println("there is an output");
+						fos.write(windowbyte, 0, sequence - start_sequence);
+						bb.clear();
+						start_sequence = ack_num;
+						ack_num += content_length;
+						window_buffer.clear();
+						window_buffer.put(sequence, content_length);
+
+						byte[]newbyte = Arrays.copyOfRange(data, 24, content_length + 24);
+						System.arraycopy(newbyte, 0, windowbyte, sequence - start_sequence, content_length);
+
+					} else if (end!=1&&(sequence != ack_num) && (sequence - start_sequence + content_length < WD_SIZE)) {//put in jump
+
+						byte[]newbyte = Arrays.copyOfRange(data, 24, content_length + 24);
+						System.arraycopy(newbyte, 0, windowbyte, sequence - start_sequence, content_length);
+
+						window_buffer.put(sequence, content_length);
+					} else if (end!=1&&(sequence != ack_num) &&( sequence - start_sequence + content_length > WD_SIZE)) {//discard future packets
+						System.out.println("sequence"+sequence);
+						System.out.println("ack_NUM"+ack_num);
+						System.out.println("content_length"+content_length);
+						System.out.println("sequence minus startsequence" + (sequence - start_sequence));
+						System.out.println("WD_SIZE" + WD_SIZE);
+						;
+					}
+					if (end == 1) {
+						end_sequence = sequence;
+					}
+					if (ack_num == end_sequence) {
+						fos.write(windowbyte, 0, sequence - start_sequence);
+						bb.clear();
+						window_buffer.clear();
+						fos.close();
+						break;
+					}
 				}
-				else {
-					returnResponse(DatagramPacket pkt);
-				}
-				DatagramPacket ack = new DatagramPacket(new byte[0], 0, 0,
-						pkt.getSocketAddress());
-				sk.send(ack);
-			}	
+			//	returnResponse(pkt, data, sk);
+			}
+			//endResponse(pkt, data, sk);
 		}
 	}
 
@@ -108,8 +171,53 @@ public class FileReceiver {
 	    }
 	    return new String(hexChars);
 	}
-	public static void returnResponse(DatagramPacket pkt, byte[]data){
-		
+	public static void returnResponse(DatagramPacket pkt, byte[]data, DatagramSocket sk){
+		ByteBuffer b = ByteBuffer.wrap(data);
+		b.clear();
+		b.putLong(0);
+		b.putInt(sequence);//useless
+		b.putInt(ack_num);
+		int window_and_bitnumber = ack << 24 | syn << 19 | fin << 18 | title << 17 | end << 16 | (window & 0xffff);//useless
+		b.putInt(window_and_bitnumber);
+		b.putInt(0);
+		CRC32 crc = new CRC32();
+		crc.reset();
+		crc.update(data, 8, data.length - 8);
+		long chksum = crc.getValue();
+		b.rewind();
+		b.putLong(chksum);
+		DatagramPacket ack = new DatagramPacket(data, data.length,				pkt.getSocketAddress());
+		try{
+		sk.send(ack);
+		}
+		catch (Exception e){
+			System.out.print("cannot send response packet");
+		}
+
+	}
+	public static void endResponse(DatagramPacket pkt, byte[]data, DatagramSocket sk){
+		ByteBuffer b = ByteBuffer.wrap(data);
+		b.clear();
+		b.putLong(0);
+		b.putInt(sequence);//useless
+		b.putInt(ack_num);
+		end = 1;
+		int window_and_bitnumber = ack << 24 | syn << 19 | fin << 18 | title << 17 | end << 16 | (window & 0xffff);//useless
+		b.putInt(window_and_bitnumber);
+		b.putInt(0);
+		CRC32 crc = new CRC32();
+		crc.reset();
+		crc.update(data, 8, data.length - 8);
+		long chksum = crc.getValue();
+		b.rewind();
+		b.putLong(chksum);
+		DatagramPacket ack = new DatagramPacket(data, data.length,				pkt.getSocketAddress());
+		try{
+			sk.send(ack);
+		}
+		catch (Exception e){
+			System.out.print("cannot send end packet");
+		}
 
 	}
 
--- Reliable-File-Transfer-Protocal/src/FileSender.java
+++ Reliable-File-Transfer-Protocal/src/FileSender.java
@@ -9,7 +9,7 @@ import java.util.zip.*;
 
 public class FileSender {
 
-	//total length of header: 22bytes
+	//total length of header: 24bytes
 	//the long field
 	private static long checksum;
 	//the int field
@@ -19,7 +19,6 @@ public class FileSender {
 	private static int content_length;
 	//the short field
 	private static short window;
-	private static Set<Integer> receive_ack_num;
 	//the bit field
 	private static short ack;
 	private static short fin;
@@ -28,11 +27,14 @@ public class FileSender {
 	private static short end;
 	//other necessary data structures for fast retransmission
     private static Map<Integer, byte[]> map;
+	private static Map<Integer, Integer> mapoftimes;
 	private static Set<Integer> ack_set;
 	//status of the sender
 	private static int status;//0: sent syn 1: receive syn 2: sent ack and get connected 4: sent fin 5: receive fin
 	//shared resources
 	private static String filename;
+
+	private static String dst_filename;
 	private static FileInputStream fis;
 	private static DatagramSocket sk;
 	private static DatagramPacket pkt;
@@ -42,23 +44,31 @@ public class FileSender {
 	private static byte[] file_array;
 	private static byte[] data;
 	private static ByteBuffer b;
+
+	private static ByteBuffer bb;
 	public static void main(String[] args) throws Exception 
 	{
-		if (args.length != 3) {
-			System.err.println("Usage: FileSender <host> <port> <file_name>");
+		if (args.length != 4) {
+			System.err.println("Usage: FileSender <host> <port> <src_file_name> <dest_file_name>");
 			System.exit(-1);
 		}
+		map = new HashMap<Integer, byte[]>();
+		mapoftimes = new HashMap<Integer, Integer>();
 		addr = new InetSocketAddress(args[0], Integer.parseInt(args[1]));
 		filename = args[2];
+		dst_filename = args[3];
 		fis = new FileInputStream(filename);
+		System.out.println(fis==null);
 		sk = new DatagramSocket();
 		WD_SIZE = (short)(5*PKT_SIZE);
-		PKT_SIZE = 978;
-		data = new byte[PKT_SIZE+22];
+		PKT_SIZE = 800;
+		data = new byte[PKT_SIZE+24];
 		file_array = new byte[PKT_SIZE];
 		b = ByteBuffer.wrap(data);
+		bb = ByteBuffer.wrap(file_array);
 		status = 0;
-
+		StartConnection();
+		SendFile();
 	}
 
 	final protected static char[] hexArray = "0123456789ABCDEF".toCharArray();
@@ -80,12 +90,12 @@ public class FileSender {
 		//some initialization of header
 		sequence = ack_num = 0;
 		window = WD_SIZE;
-		receive_ack_num.clear();
 		ack = fin = syn = end = 0;
 		title = 1;
 		int count = 0;
 		try {
-			content_length = filename.length();
+
+			content_length = dst_filename.length();
 			b.putLong(0);
 			b.putInt(sequence);//sequence of the file excl. header
 			b.putInt(0);
@@ -93,22 +103,30 @@ public class FileSender {
 			b.putInt(window_and_bitnumber);
 			b.putInt(content_length);
 			sequence += content_length;
-			file_array = filename.getBytes();
-			b.put(file_array);
+			b.put(dst_filename.getBytes());
 			crc.reset();
 			crc.update(data, 8, data.length - 8);
 			long chksum = crc.getValue();
 			b.rewind();
 			b.putLong(chksum);
+			System.out.println("content-length"+content_length);
 
 			pkt = new DatagramPacket(data, data.length, addr);
+			System.out.println("length"+data.length);
+
 			// Debug output
 			//System.out.println("Sent CRC:" + chksum + " Contents:" + bytesToHex(data));
 			sk.send(pkt);
+
+			System.out.println("title package prepared");
 			title = 0;
-			sequence += 22+filename.length();
 			while (true) {
+				b.clear();
+				bb.clear();
+
+				System.out.println("file_array size:"+file_array.length+" PKT_SIZE: "+PKT_SIZE);
 				if((content_length = fis.read(file_array, 0, PKT_SIZE)) != -1) {
+					System.out.println("package sent:"+count);
 					b.clear();
 					// reserve space for checksum
 					b.putLong(0);
@@ -117,6 +135,7 @@ public class FileSender {
 					window_and_bitnumber = ack << 22 | syn << 19 | fin << 18 | title << 17 | end << 16 | (window & 0xffff);
 					b.putInt(window_and_bitnumber);
 					b.putInt(content_length);
+					b.put(file_array,0,content_length);
 					sequence += content_length;
 					crc.reset();
 					crc.update(data, 8, data.length - 8);
@@ -127,9 +146,14 @@ public class FileSender {
 					pkt = new DatagramPacket(data, data.length, addr);
 					// Debug output
 					//System.out.println("Sent CRC:" + chksum + " Contents:" + bytesToHex(data));
+					System.out.println("pktsize"+pkt.getLength());
 					sk.send(pkt);
+
+					System.out.println("sent"+count);
 					map.put(sequence, data);//to be cleared once received acknum = sequence+1
+
 					count++;
+
 				}
 				else{
 					end = 1;
@@ -141,7 +165,7 @@ public class FileSender {
 					window_and_bitnumber = ack << 20 | syn << 19 | fin << 18 | title << 17 | end << 16 | (window & 0xffff);
 					b.putInt(window_and_bitnumber);
 					b.putInt(content_length);
-					sequence += 22 + content_length;
+					sequence += 24 + content_length;
 					crc.reset();
 					crc.update(data, 8, data.length - 8);
 					chksum = crc.getValue();
@@ -152,11 +176,14 @@ public class FileSender {
 					// Debug output
 					//System.out.println("Sent CRC:" + chksum + " Contents:" + bytesToHex(data));
 					sk.send(pkt);
+
 					map.put(sequence, data);//to be cleared once received acknum = sequence+1
 					count++;
+					break;
 				}
-				System.out.println("checksum:"+chksum);
-				System.out.println("packet:"+count);
+				//System.out.println("checksum:"+chksum);
+				//System.out.println("packet:"+count);
+
 			}
 		}catch (Exception e){
 			System.out.println("cannot send file");
